409b13a3b4508cfe2b2501756b65833c685cdb4c
Jonathan12502 <jonathan.shah@icloud.com>
2022-08-01 17:05:30 -0400

    changes to project files

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic_name from "../assets/Jonathan Shah - IBM Accelerate Headshot.PNG";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {profile_pic_name}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        
+        <div className="name_title"><center>Jonathan Shah</center></div>
+        <div className="brief_description">
+          <center>Student studying computer science at Binghamton University</center>
         </div>
       </div>
-    );
+    </div>
+  </div> 
+      </div>
+    )
   }
 }
